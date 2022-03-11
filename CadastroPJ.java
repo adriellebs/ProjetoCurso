c0d21af605d69e0dd266bd2644b584e6fa057a57
Adrielle Barros <99371469+adriellebs@users.noreply.github.com>
2022-03-10 23:12:00 -0300

    Add files via upload

---
+++ CadastroPJ.java
@@ -0,0 +1,37 @@
+package Projeto;
+
+public class CadastroPJ extends ProjetoCurso{
+	
+	private int cnpj;
+	private String segmento;
+	
+
+	public CadastroPJ(String nome, String endereco, int telefone, 
+			int cnpj, String segmento) {
+		super(nome, endereco, telefone);
+		this.cnpj = cnpj;
+		this.segmento = segmento;
+		
+	}
+
+
+	public int getCnpj() {
+		return cnpj;
+	}
+
+
+	public void setCnpj(int cnpj) {
+		this.cnpj = cnpj;
+	}
+
+
+	public String getSegmento() {
+		return segmento;
+	}
+
+
+	public void setSegmento(String segmento) {
+		this.segmento = segmento;
+	}
+
+}
